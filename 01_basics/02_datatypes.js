1aac320299ca69795f17610510ef97be0d1f6202
PRANJAL-MISHRA123 <111454754+PRANJAL-MISHRA123@users.noreply.github.com>
2023-09-08 17:18:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS code as newer version 
+
+//alert(3 + 3) // This will give error because we are using Node.js not browser
+
+//List of datatypes
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+let age = 18
+
+console.log(typeof(age))
+console.log(typeof null) // => will show null as object
+console.log(typeof undefined) // => will show output undefined
+
