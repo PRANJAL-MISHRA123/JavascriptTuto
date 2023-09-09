078d28c6b4a56bcf2ce167de284fb7d9498bf2c4
PRANJAL-MISHRA123 <111454754+PRANJAL-MISHRA123@users.noreply.github.com>
2023-09-09 16:50:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,36 @@
+
+let score = 45;
+let strScore = "45"
+let score2 = "34abc"
+let score3 = null
+let score4 = undefined
+
+// console.log(typeof score)
+// console.log(typeof strScore)
+
+// console.log();
+
+
+let valueInNumber = Number(strScore)
+let value2 = Number(score2)
+let value3 = Number(score3)
+let value4 = Number(score4)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof value2)
+// console.log(typeof value3)
+
+// console.log(valueInNumber)
+// console.log(value2)
+// console.log(value3)
+// console.log(value4)
+
+let bl = ""
+let bl2 = "Nigha"
+
+let blInBoolean = Boolean(bl)
+let bl2InBoolean = Boolean(bl2)
+
+console.log(blInBoolean)
+console.log(bl2InBoolean)
+
