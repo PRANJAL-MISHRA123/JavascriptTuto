34c031bdfe5f54d56bfc06cfceec0370738745d9
PRANJAL-MISHRA123 <111454754+PRANJAL-MISHRA123@users.noreply.github.com>
2023-09-26 14:47:27 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+
+// if(2 === "2")
+// {
+//     console.log("True");
+// }
+
+// else{
+//     console.log("False");
+// }
+
+const score = 200
+
+// if(score == 200){
+//     let power = "fly"
+//     console.log(`User Power : ${power}`);
+// }
+
+// console.log(`User Power : ${power}`);
+
+const balance = 1000;
+
+if(balance > 100 ) console.log("Greater than 100");
