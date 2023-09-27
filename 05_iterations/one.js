36609181fddfde47063623cba0223d085dc93f64
PRANJAL-MISHRA123 <111454754+PRANJAL-MISHRA123@users.noreply.github.com>
2023-09-27 18:45:24 +0000

    Loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+
+// for(i = 0 ; i <= 10; i++){
+    
+//     if(i == 5)
+//     {
+//         console.log("5 is the best number");
+//     }
+//     console.log(i);
+// }
+
+// for( i = 0 ; i <= 3; i++)
+// {
+//     for(j = 0 ; j <= i; j++){
+//         console.log(`j = ${j} and i = ${i}`);
+//     }
+// }
+
+let myArray = ["Sukuna", "Gojo", "Kenjaku"]
+
+for( let i = 0 ; i < myArray.length; i++){
+    console.log(myArray[i]);
+}
---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+
+let myArray = ["Sukuna", "Gojo", "Kenjaku"];
+
+let i = 0
+while(i < myArray.length){
+    console.log(myArray[i]);
+    i++;
+}
+
+let score = 1
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10)
